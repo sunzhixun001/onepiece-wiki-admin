6a93cdf178b91ab5abd03ac82b106e8783d24aeb
sunzhixun <250994562@qq.com>
2019-10-30 19:29:08 +0800

    1030

--- src/api/access/index.ts
+++ src/api/access/index.ts
@@ -5,35 +5,56 @@ const axios = require('axios');
 interface TokenValue {
     token: string;
     expires: number;
-}
+};
 
-// 接口调用凭据
+// 获取接口调用凭据
 const getAccessToken = async (): Promise<string> => {
     const value: string | null = localStorage.getItem("AccessToken");
     if( value === null ) {
-        const now: number = new Date().getTime();
-        const response = await axios.get(GET_ACCESS_TOKEN, {
-            params: {
-                appid: appId,
-                secret: Secret,
-                grant_type: 'client_credential'
-            }
-        });
-        console.log("接口调用凭据", response);
-        const { data, status, statusText } = response;   
-        const { access_token, expires_in } = data;
-        if (status === 200 && statusText === "OK") {
-            localStorage.setItem("AccessToken", JSON.stringify({
-                token: access_token,
-                expires: now + (expires_in * 1000)
-            }));
-            return access_token;
-        }
-        return '';
+        fetchGetAccessToken().then(token => {
+
+        }).catch( error => {
+            console.log("接口调用凭据出错", error);
+        })
     }
     const obj: TokenValue = JSON.parse(value as string);
+    const {token, expires} = obj;
+    if(new Date().getTime() > expires) {
+        console.log("access_token已过期");
+        fetchGetAccessToken().then(token => {
+
+        }).catch( error => {
+            console.log("接口调用凭据出错", error);
+        })
+    }
     return obj.token;
-}
+};
+
+// 从腾讯服务器获取调用凭据
+const fetchGetAccessToken = async (): Promise<string> => {
+    const now: number = new Date().getTime();
+    const response = await axios.get(GET_ACCESS_TOKEN, {
+        params: {
+            // appid: appId,
+            // secret: Secret,
+            // grant_type: 'client_credential'
+        }
+    });
+    console.log("接口调用凭据", response);
+    const { data, status, statusText } = response;   
+    const { access_token, expires_in, errcode, errmsg } = data;
+    if (status !== 200 || statusText !== "OK") {
+        throw new Error('error');
+    }
+    if(errcode !== 0 || errmsg !== "ok") {
+        throw new Error(`${errcode}: ${errmsg}`);
+    }
+    localStorage.setItem("AccessToken", JSON.stringify({
+        token: access_token,
+        expires: now + (expires_in * 1000)
+    }));  
+    return access_token;
+};
 
 const getTokenExpires = (): string => {
     const value: string | null = localStorage.getItem("time");
@@ -42,9 +63,9 @@ const getTokenExpires = (): string => {
     // let date = new Date(value);
     // date.setHours(date.getHours() + 2);
     // return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-}
+};
 
 export {
     getTokenExpires,
     getAccessToken
-}
+};
